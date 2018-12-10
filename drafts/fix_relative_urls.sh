6b52b66227bf952bcc711edbfccd3b47164156cb
Sam White <samuel.j.white@gmail.com>
2018-12-10 07:40:42 -0800

    fix URLs

--- drafts/fix_relative_urls.sh
+++ drafts/fix_relative_urls.sh
@@ -6,7 +6,6 @@ do
   for match in ${matches[@]}
     do
     mod_match_no_brackets=$(echo "${match}" | sed 's_\](__g')
-    mod_match_no_slahes=$(echo "${mod_match_no_brackets}" | sed 's_/_-_g')
-    sed -i.bak "\_${match}_ s%${match}%\](https://robertslab.github.io/sams-notebook/${mod_match_no_slahes}%g" ${file}
+    sed -i.bak "\_${match}_ s%${match}%\](https://robertslab.github.io/sams-notebook/${mod_match_no_brackets}%g" ${file}
   done
 done
