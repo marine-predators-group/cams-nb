4c6d159523bcbd3a1680dff7f552dff468fc6d78
Sam White <samuel.j.white@gmail.com>
2019-01-09 08:17:19 -0800

    update variables

--- sbatch_scripts/20190108_oly_maker_functional_annotation.sh
+++ sbatch_scripts/20190108_oly_maker_functional_annotation.sh
@@ -41,43 +41,50 @@ echo ${PATH} | tr : \\n >> system_path.log
 maker_dir=/gscratch/srlab/programs/maker-2.31.10/bin
 
 maker_prot_fasta=/gscratch/scrubbed/samwhite/outputs/20190108_oly_maker_id_mapping/20181127_oly_genome_snap02.all.maker.proteins.renamed.fasta
+maker_put_prot_fasta=20181127_oly_genome_snap02.all.maker.proteins.renamed.putative_function.fasta
 maker_transcripts_fasta=/gscratch/scrubbed/samwhite/outputs/20190108_oly_maker_id_mapping/20181127_oly_genome_snap02.all.maker.transcripts.renamed.fasta
+maker_put_transcripts_fasta=20181127_oly_genome_snap02.all.maker.transcripts.renamed.putative_function.fasta
 snap02_gff=/gscratch/scrubbed/samwhite/outputs/20190108_oly_maker_id_mapping/20181127_oly_genome_snap02.all.renamed.gff
+snap02_put_gff=20181127_oly_genome_snap02.all.renamed.putative_function.gff
+snap02_put_domains_gff=20181127_oly_genome_snap02.all.renamed.putative_function.domain_added.gff
+snap02_put_domains_visible_gff=20181127_oly_genome_snap02.all.renamed.visible_ips_domains.gff
 blastp_out=/gscratch/scrubbed/samwhite/outputs/20190108_oly_maker_blastp/20190108_blastp.outfmt6
 maker_ips=/gscratch/scrubbed/samwhite/outputs/20190108_oly_maker_interproscan/20190108_oly_maker_proteins_ips.tsv
 sp_db=/gscratch/srlab/blastdbs/UniProtKB_20181008/20190108_uniprot_sprot.dat
+
+
 
 ## Add putative gene functions
 ### GFF
 ${maker_dir}/maker_functional_gff \
 ${sp_db} \
 ${blastp_out} \
-20181127_oly_genome_snap02.all.renamed.gff \
-> 20181127_oly_genome_snap02.all.renamed.putative_function.gff
+${snap02_gff} \
+> ${snap02_put_gff}
 
 ### Proteins
 ${maker_dir}/maker_functional_fasta \
 ${sp_db} \
 ${blastp_out} \
-20181127_oly_genome_snap02.all.maker.proteins.renamed.fasta \
-> 20181127_oly_genome_snap02.all.maker.proteins.renamed.putative_function.fasta
+${maker_prot_fasta} \
+> ${maker_put_prot_fasta}
 
 ### Transcripts
 ${maker_dir}/maker_functional_fasta \
 ${sp_db} \
 ${blastp_out} \
-20181127_oly_genome_snap02.all.maker.transcripts.renamed.fasta \
-> 20181127_oly_genome_snap02.all.maker.transcripts.renamed.putative_function.fasta
+${maker_transcripts_fasta} \
+> ${maker_put_transcripts_fasta}
 
 ## Add InterProScan domain info
 ### Add searchable tags
 ${maker_dir}/ipr_update_gff \
-20181127_oly_genome_snap02.all.renamed.putative_function.gff \
-20181127_oly_maker_proteins_ips.renamed.tsv \
-> 20181127_oly_genome_snap02.all.renamed.putative_function.domain_added.gff
+${snap02_put_gff} \
+${maker_ips} \
+> ${snap02_put_domains_gff}
 
 ### Add viewable features for genome browsers (JBrowse, Gbrowse, Web Apollo)
 ${maker_dir}/iprscan2gff3 \
-20181127_oly_maker_proteins_ips.renamed.tsv \
-20181127_oly_genome_snap02.all.renamed.gff \
-> 20181127_oly_genome_snap02.all.renamed.visible_ips_domains.gff
+${maker_ips} \
+${snap02_gff} \
+> ${snap02_put_domains_visible_gff}
