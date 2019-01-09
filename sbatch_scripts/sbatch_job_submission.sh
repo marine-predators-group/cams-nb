6f80b303ffe86ee5f6139cba5c2655c36536e240
Sam White <samuel.j.white@gmail.com>
2019-01-09 09:56:07 -0800

    add comments

--- sbatch_scripts/sbatch_job_submission.sh
+++ sbatch_scripts/sbatch_job_submission.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-
+# Read in user-supplied file
+echo "Enter path to SBATCH script file:"
 read sbatch_script
 
-
+# Extract account and partition names from suppllied SBATCH script.
 account=$(grep "\-\-account" ${sbatch_script} | awk -F "=" '{print $2}')
 partition=$(grep "\-\-partition" ${sbatch_script} | awk -F "=" '{print $2}')
 
+# Execute SBATCH job submission.
 sbatch \
 -p ${partition} \
 -A ${account} \
