fae5aa3818f9e6327c2a745ca0d608994c770bb7
sam <samwhite@uw.edu>
2019-02-12 10:21:15 -0800

    Fix input FastA file name.

--- sbatch_scripts/20190213_geoduck_maker_genome_annotation.sh
+++ sbatch_scripts/20190213_geoduck_maker_genome_annotation.sh
@@ -95,7 +95,7 @@ interproscan=/gscratch/srlab/programs/interproscan-5.31-70.0/interproscan.sh
 maker_opts_file=./maker_opts.ctl
 
 ### Path to genome FastA file
-genome=/gscratch/srlab/sam/data/P_generosa/generosa_genomes/Pgenerosa_v071.fa
+genome=/gscratch/srlab/sam/data/P_generosa/generosa_genomes/Pgenerosa_v071.fasta
 
 ### Path to transcriptome FastA file
 transcriptome=/gscratch/srlab/sam/data/P_generosa/generosa_transcriptomes/20180827_trinity_geoduck.fasta
