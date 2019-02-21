07942cd8e8af81cd052081b78b26e710defa160c
sam <samwhite@uw.edu>
2019-02-21 09:45:04 -0800

    Initial script

---
+++ sbatch_scripts/20190221_cgigas_genome_prep_bismark.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+## Job Name
+#SBATCH --job-name=bismark
+## Allocation Definition
+#SBATCH --account=coenv
+#SBATCH --partition=coenv
+## Resources
+## Nodes
+#SBATCH --nodes=2
+## Walltime (days-hours:minutes:seconds format)
+#SBATCH --time=10-00:00:00
+## Memory per node
+#SBATCH --mem=120G
+##turn on e-mail notification
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=samwhite@uw.edu
+## Specify the working directory for this job
+#SBATCH --workdir=20190221_cgigas_genome_prep_bismark
+
+# Load Python Mox module for Python module availability
+
+module load intel-python3_2017
+
+# Document programs in PATH (primarily for program version ID)
+
+date >> system_path.log
+echo "" >> system_path.log
+echo "System PATH for $SLURM_JOB_ID" >> system_path.log
+echo "" >> system_path.log
+printf "%0.s-" {1..10} >> system_path.log
+echo ${PATH} | tr : \\n >> system_path.log
+
+
+# Directories and programs
+## Programs
+bismark_dir="/gscratch/srlab/programs/Bismark-0.19.0"
+bowtie2_dir="/gscratch/srlab/programs/bowtie2-2.3.4.1-linux-x86_64"
+samtools="/gscratch/srlab/programs/samtools-1.9/samtools"
+
+## Files
+genome="/gscratch/srlab/sam/data/C_gigas/genomes/Crassostrea_gigas.oyster_v9.dna_sm.toplevel.fa"
