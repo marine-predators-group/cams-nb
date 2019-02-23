6423b28abad080412f0a4545cb17d3df907fcb60
sam <samwhite@uw.edu>
2019-02-23 13:46:53 -0800

    fix workding dir path

--- sbatch_scripts/20190222_cgigas_pe_bismark.sh
+++ sbatch_scripts/20190222_cgigas_pe_bismark.sh
@@ -15,7 +15,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=samwhite@uw.edu
 ## Specify the working directory for this job
-#SBATCH --workdir=gscratch/scrubbed/samwhite/outputs/20190222_cgigas_pe_bismark
+#SBATCH --workdir=/gscratch/scrubbed/samwhite/outputs/20190222_cgigas_pe_bismark
 
 # Load Python Mox module for Python module availability
 
