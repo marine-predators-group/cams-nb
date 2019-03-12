399a80d3e1ff5589aacccb816c3cadae6315a69d
Sam White <samuel.j.white@gmail.com>
2019-03-12 12:08:47 -0700

    set R1 and R2 arrays

--- sbatch_scripts/20190312_cvir_gonad_bismark.sh
+++ sbatch_scripts/20190312_cvir_gonad_bismark.sh
@@ -49,3 +49,31 @@ R2="${wd}/cvir_bsseq_all_pe_R2.fastq.gz"
 ## FastQ files lists
 R1_list="${wd}/cvir_bsseq_pe_all_R1.list"
 R2_list="${wd}/cvir_bsseq_pe_all_R2.list"
+
+## Save FastQ files to arrays
+R1_array=(${reads_dir}/*_R1_*.fq.gz)
+R2_array=(${reads_dir}/*_R2_*.fq.gz)
+
+# Check for existence of previous concatenation
+# If they exist, delete them
+
+for file in ${R1} ${R1_list} ${R2} ${R2_list}
+do
+  if [ -e ${file} ]; then
+    rm ${file}
+  fi
+done
+
+# Concatenate R1 reads and generate lists of FastQs
+for fastq in ${reads_dir}*R1*.gz
+do
+  echo ${fastq} >> ${R1_list}
+  cat ${fastq} >> ${R1}
+done
+
+# Concatenate R2 reads and generate lists of FastQs
+for fastq in ${reads_dir}*R2*.gz
+do
+  echo ${fastq} >> ${R2_list}
+  cat ${fastq} >> ${R2}
+done
