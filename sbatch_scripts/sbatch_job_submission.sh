50bb00a68f6e6443d623580e69b4729c5258a003
Sam White <samuel.j.white@gmail.com>
2019-03-18 21:38:11 -0700

    revert to previous version

--- sbatch_scripts/sbatch_job_submission.sh
+++ sbatch_scripts/sbatch_job_submission.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
+# Read in user-supplied file
+echo "Enter path to SBATCH script file:"
+read sbatch_script
+
 # Extract account and partition names from suppllied SBATCH script.
-## Back slashes escape the hyphens
-## Parses out account (e.g. srlab or coenv)
-## Parses out partition (e.g. srlab or coenv)
-script_location=$(grep "\-\-workdir" | awk -F "=" '{print $2}')
 account=$(grep "\-\-account" ${sbatch_script} | awk -F "=" '{print $2}')
 partition=$(grep "\-\-partition" ${sbatch_script} | awk -F "=" '{print $2}')
 
-# Complete name of script_location
-sbatch_script=${script_location}.sh
-
 # Execute SBATCH job submission.
-## Submits job to account/partition specified in script
 sbatch \
 -p ${partition} \
 -A ${account} \
