81747827a460ddb3d6606db887a4b285c44317b4
Sam White <samuel.j.white@gmail.com>
2019-03-25 12:07:42 -0700

    initial commit

---
+++ sbatch_scripts/20190325_blastn_metagenomics_geoduck_metagenemark.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+## Job Name
+#SBATCH --job-name=blastx_ctendia
+## Allocation Definition
+#SBATCH --account=coenv
+#SBATCH --partition=coenv
+## Resources
+## Nodes
+#SBATCH --nodes=1
+## Walltime (days-hours:minutes:seconds format)
+#SBATCH --time=25-00:00:00
+## Memory per node
+#SBATCH --mem=120G
+##turn on e-mail notification
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=samwhite@uw.edu
+## Specify the working directory for this job
+#SBATCH --workdir=/gscratch/scrubbed/samwhite/outputs/20190318_blastx_geoduck_ctenidia_RNAseq
+
+# Load Python Mox module for Python module availability
+
+module load intel-python3_2017
+
+# Document programs in PATH (primarily for program version ID)
+
+date >> system_path.log
+echo "" >> system_path.log
+echo "System PATH for $SLURM_JOB_ID" >> system_path.log
+echo "" >> system_path.log
+printf "%0.s-" {1..10} >> system_path.log
+echo ${PATH} | tr : \\n >> system_path.log
+
+
+wd="$(pwd)"
+
+
+# Paths to input/output files
+blastn_out="${wd}/blastn.outfmt6"
+db=""
+fasta=""
+
+# Paths to programs
+blast_dir="/gscratch/srlab/programs/ncbi-blast-2.8.1+/bin"
+blastn="${blast_dir}/blastn"
+
+
+# Run blastx on Trinity fasta
+${blastn} \
+-query ${fasta} \
+-db ${db} \
+-max_target_seqs 1 \
+-outfmt "6 std staxids" \
+-evalue 1e-10 \
+-num_threads 28 \
+> ${blastn_out}
