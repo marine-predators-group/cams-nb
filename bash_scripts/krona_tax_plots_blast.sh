b8464b42d076e2fc71d61fd5070b63a9e887e270
Sam White <samuel.j.white@gmail.com>
2019-03-25 14:51:28 -0700

    add awk and krona commands

--- bash_scripts/krona_tax_plots_blast.sh
+++ bash_scripts/krona_tax_plots_blast.sh
@@ -1,11 +1,21 @@
 #!/bin/env bash
 
-# Bash script for creating Krona plot of metagenomics taxonomies.
+# Bash script for creating Krona plot of metagenomics taxonomies from BLAST outputs.
+
+# BLAST output format is expected to be: "6 std staxids"
 
 # Input/output files
-blast_out=""
-krona_out=""
-krona_tax_list=""
+blast_out="blastp.outfmt6"
+krona_out="krona_megahit_MGM_blastp"
+krona_tax_list="krona_tax.lst"
 
 # Programs
-krona=""
+krona="/home/sam/programs/KronaTools-2.7/bin/ktImportTaxonomy"
+
+## Extract NCBI taxon IDs from BLAST output
+### Set ';' and tab as field delimiters. Prevents issues with taxon ID column having multiple entries for multi-strain matching
+### Prints a tab-delimited ouptut file containing the query ID and the taxon ID
+awk -F'[;\t]' '{print $1 "\t" $13}' ${blast_out} > ${krona_tax_list}
+
+## Create Krona plot, specifying output filename
+${krona} -o ${krona_out}
