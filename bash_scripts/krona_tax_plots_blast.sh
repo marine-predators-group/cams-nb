0ac1a4ce5b824d3b5ad6cdbe38745d7812dde64b
Sam White <samuel.j.white@gmail.com>
2019-03-26 09:04:10 -0700

    modify awk to print uniques based on query and tax ID

--- bash_scripts/krona_tax_plots_blast.sh
+++ bash_scripts/krona_tax_plots_blast.sh
@@ -8,23 +8,25 @@
 blast_out="blastp.outfmt6"
 krona_out="krona_megahit_MGM_blastp.html"
 krona_tax_list="krona_tax.lst"
+krona_err="krona_stderr.txt" # Krona standard error capture
+krona_out="krona_stdout.txt" # Krona standard out capture
 
 # Programs
 krona="/home/sam/programs/KronaTools-2.7/bin/ktImportTaxonomy"
 
 ## Extract NCBI taxon IDs from BLAST output
-### Uses awk associative array to pull out unique entries with highest bitscore
+### Uses awk associative array to pull out unique entries with highest bitscore and unqique NCBI tax IDs
 ### BLAST output is sorted by bitscore for multiple matches for a single query
 ### Pipe unique entries to second awk command
 ### Set ';' and tab as field delimiters. Prevents issues with taxon ID column having multiple entries for multi-strain matching
 ### Prints a tab-delimited ouptut file containing the query ID and the taxon ID
-awk '!seen[$1]++'  ${blast_out} \
-| awk -F'[;\t]' '{print $1 "\t" $13}' \
+awk -F'[;\t]' '!seen[$1,$13]++' ${blast_out} \
+| awk '{print $1 "\t" $13}' \
 > ${krona_tax_list}
 
 ## Create Krona plot, specifying output filename
 ${krona} \
 -o ${krona_out} \
 ${krona_tax_list} \
-1> stdout.txt \
-2> stderr.txt
+1> ${krona_out} \
+2> ${krona_err}
