d231bf9962d855f455ad32b7c8686684291f3202
Sam White <samuel.j.white@gmail.com>
2019-03-26 08:16:16 -0700

    add awk command to pull out top query match, based on bitscore

--- bash_scripts/krona_tax_plots_blast.sh
+++ bash_scripts/krona_tax_plots_blast.sh
@@ -13,9 +13,14 @@ krona_tax_list="krona_tax.lst"
 krona="/home/sam/programs/KronaTools-2.7/bin/ktImportTaxonomy"
 
 ## Extract NCBI taxon IDs from BLAST output
+### Uses awk associative array to pull out unique entries with highest bitscore
+### BLAST output is sorted by bitscore for multiple matches for a single query
+### Pipe unique entries to second awk command
 ### Set ';' and tab as field delimiters. Prevents issues with taxon ID column having multiple entries for multi-strain matching
 ### Prints a tab-delimited ouptut file containing the query ID and the taxon ID
-awk -F'[;\t]' '{print $1 "\t" $13}' ${blast_out} > ${krona_tax_list}
+awk '!seen[$1]++'  ${blast_out} \
+| awk -F'[;\t]' '{print $1 "\t" $13}' \
+> ${krona_tax_list}
 
 ## Create Krona plot, specifying output filename
 ${krona} \
