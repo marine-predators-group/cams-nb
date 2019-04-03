75a206d939c51b248fc072d25b27d91fc2a93c94
Sam White <samuel.j.white@gmail.com>
2019-04-03 06:20:01 -0700

    improve comments

--- bash_scripts/sbatch_job_submission.sh
+++ bash_scripts/sbatch_job_submission.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# A script to simplify sbatch job submission
+# Run script in the following fashion:
+# bash sbatch_job_submission.sh <sbatch script file>
+
+# Script will automatically submit to proper SBATCH account/partition
+# based on what's entered in the script header.
 
 # Read in user-supplied file
 sbatch_script="$1"
