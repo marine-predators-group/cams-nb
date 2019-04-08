a8391dbbbc3cfa22aaa5c4dea7ae883784f68699
Sam White <samuel.j.white@gmail.com>
2019-04-08 15:46:23 -0700

    initial commit

---
+++ sbatch_scripts/20190409_trinity_pgen_EPI115_RNAseq.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+## Job Name
+#SBATCH --job-name=trinity_20190215
+## Allocation Definition
+#SBATCH --account=coenv
+#SBATCH --partition=coenv
+## Resources
+## Nodes
+#SBATCH --nodes=2
+## Walltime (days-hours:minutes:seconds format)
+#SBATCH --time=5-00:00:00
+## Memory per node
+#SBATCH --mem=120G
+##turn on e-mail notification
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=samwhite@uw.edu
+## Specify the working directory for this job
+#SBATCH --workdir=/gscratch/scrubbed/samwhite/outputs/20190409_trinity_pgen_EPI115_RNAseq.sh
+
+# Load Python Mox module for Python module availability
+
+module load intel-python3_2017
+
+# Document programs in PATH (primarily for program version ID)
+
+date >> system_path.log
+echo "" >> system_path.log
+echo "System PATH for $SLURM_JOB_ID" >> system_path.log
+echo "" >> system_path.log
+printf "%0.s-" {1..10} >> system_path.log
+echo ${PATH} | tr : \\n >> system_path.log
+
+data_dir=/gscratch/scrubbed/samwhite/data/P_generosa/RNAseq
+trinity_dir=/gscratch/srlab/programs/Trinity-v2.8.3
+assembly_stats=assembly_stats.txt
