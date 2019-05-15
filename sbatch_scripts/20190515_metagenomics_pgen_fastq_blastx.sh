2c2ca67274bff9a30da5eee4354850a1d1941151
Sam White <samuel.j.white@gmail.com>
2019-05-15 12:59:02 -0700

    initial commit

---
+++ sbatch_scripts/20190515_metagenomics_pgen_fastq_blastx.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+## Job Name
+#SBATCH --job-name=blastn_metagenomics
+## Allocation Definition
+#SBATCH --account=coenv
+#SBATCH --partition=coenv
+## Resources
+## Nodes
+#SBATCH --nodes=1
+## Walltime (days-hours:minutes:seconds format)
+#SBATCH --time=25-00:00:00
+## Memory per node
+#SBATCH --mem=120G
+##turn on e-mail notification
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=samwhite@uw.edu
+## Specify the working directory for this job
+#SBATCH --workdir=/gscratch/scrubbed/samwhite/outputs/20190416_metagenomics_pgen_blastn
+
+# Exit script if any command fails
+set -e
+
+# Load Python Mox module for Python module availability
+
+module load intel-python3_2017
+
+# Document programs in PATH (primarily for program version ID)
+
+date >> system_path.log
+echo "" >> system_path.log
+echo "System PATH for $SLURM_JOB_ID" >> system_path.log
+echo "" >> system_path.log
+printf "%0.s-" {1..10} >> system_path.log
+echo ${PATH} | tr : \\n >> system_path.log
+
+
+wd="$(pwd)"
+threads=28
+
+# Paths to programs
+blast_dir="/gscratch/srlab/programs/ncbi-blast-2.8.1+/bin"
+blastx="${blast_dir}/blastx"
+seqtk="/gscratch/srlab/programs/seqtk-1.3/seqtk"
+
+# Paths to blastdbs
+blastdb_dir="/gscratch/srlab/blastdbs/ncbi-nr-nt-v5"
+blast_db="${blastdb_dir}/nt"
+
+
+
+
+# Export BLAST database directory
+export BLASTDB=${blastdb_dir}
+
+# Loop through FastAs
+# Create list of those FastAs for reference
+# Parse out sample names
+# Run BLASTn on each FastA
+for fasta in ${fasta_dir}/*nucleotides.fasta
+do
+  echo ${fasta} >> input.fasta.list.txt
+  no_ext=${fasta%%.*}
+  sample_name=$(echo ${no_ext##*/})
+  # Run blastx on Trinity fasta
+  ${blastn} \
+  -query ${fasta} \
+  -db ${blast_db} \
+  -max_target_seqs 1 \
+  -outfmt "6 std staxids" \
+  -evalue 1e-10 \
+  -num_threads ${threads} \
+  > ${wd}/${sample_name}.blastn.outfmt6
+done
