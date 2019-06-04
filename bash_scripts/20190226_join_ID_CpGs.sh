2ce72e726b6a5ad4ff44262a0cd6a7ce3b4193a0
Sam White <samuel.j.white@gmail.com>
2019-06-04 08:45:46 -0700

    initial commit

---
+++ bash_scripts/20190226_join_ID_CpGs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+## Script to append sample-specific headers to each ID_CpG
+## file and join all ID_CpG files.
+
+## Run file from within this directory.
+
+# Temp file placeholder
+tmp=$(mktemp)
+
+# Create array of subdirectories.
+array=(*/)
+
+# Create column headers for ID_CpG files using sample name from directory name.
+for file in ${array[@]}
+do
+  gene=$(echo ${file} | awk -F\[._] '{print $6"_"$7}')
+  sed "1iID\t${gene}" ${file}ID_CpG > ${file}ID_CpG_labelled
+done
+
+# Create initial file for joining
+cp ${array[0]}ID_CpG_labelled ID_CpG_labelled_all
+
+# Loop through array and performs joins.
+for file in ${array[@]:1}
+do
+  join \
+  --nocheck-order \
+  ID_CpG_labelled_all ${file}ID_CpG_labelled \
+  | column -t \
+  > ${tmp} \
+  && mv ${tmp} ID_CpG_labelled_all
+done
