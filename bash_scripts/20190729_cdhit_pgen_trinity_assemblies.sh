920839e36faf54ddb2f026bd267453dfec9614cc
sam <samwhite@uw.edu>
2019-07-24 15:29:55 -0700

    create assemblies_array

--- bash_scripts/20190729_cdhit_pgen_trinity_assemblies.sh
+++ bash_scripts/20190729_cdhit_pgen_trinity_assemblies.sh
@@ -5,15 +5,24 @@
 
 # Create array of directories for storing Trinity assemblies
 assembly_dirs_array=(
-/gscratch/srlab/sam/data/P_generosa/transcriptomes/20190827_assembly \
-/gscratch/srlab/sam/data/P_generosa/transcriptomes/ctenidia \
-/gscratch/srlab/sam/data/P_generosa/transcriptomes/gonad \
-/gscratch/srlab/sam/data/P_generosa/transcriptomes/heart \
-/gscratch/srlab/sam/data/P_generosa/transcriptomes/juvenile/EPI115 \
-/gscratch/srlab/sam/data/P_generosa/transcriptomes/juvenile/EPI116 \
-/gscratch/srlab/sam/data/P_generosa/transcriptomes/juvenile/EPI123 \
-/gscratch/srlab/sam/data/P_generosa/transcriptomes/juvenile/EPI124 \
+/gscratch/srlab/sam/data/P_generosa/transcriptomes/20190827_assembly
+/gscratch/srlab/sam/data/P_generosa/transcriptomes/ctenidia
+/gscratch/srlab/sam/data/P_generosa/transcriptomes/gonad
+/gscratch/srlab/sam/data/P_generosa/transcriptomes/heart
+/gscratch/srlab/sam/data/P_generosa/transcriptomes/juvenile/EPI115
+/gscratch/srlab/sam/data/P_generosa/transcriptomes/juvenile/EPI116
+/gscratch/srlab/sam/data/P_generosa/transcriptomes/juvenile/EPI123
+/gscratch/srlab/sam/data/P_generosa/transcriptomes/juvenile/EPI124
 /gscratch/srlab/sam/data/P_generosa/transcriptomes/larvae/EPI99)
 
-# Array of Trinity assemblies for rysnc-ing
-assemblies_array
+# Array of Trinity assemblies remote paths for rysnc-ing
+assemblies_array=(
+20180827_trinity_geoduck_RNAseq/Trinity.fasta
+20190409_trinity_pgen_ctenidia_RNAseq/trinity_out_dir/Trinity.fasta
+20190409_trinity_pgen_gonad_RNAseq/trinity_out_dir/Trinity.fasta
+20190215_trinity_geoduck_heart_RNAseq/trinity_out_dir/Trinity.fasta
+20190409_trinity_pgen_EPI115_RNAseq/trinity_out_dir/Trinity.fasta
+20190409_trinity_pgen_EPI116_RNAseq/trinity_out_dir/Trinity.fasta
+20190409_trinity_pgen_EPI123_RNAseq/trinity_out_dir/Trinity.fasta
+20190409_trinity_pgen_EPI124_RNAseq/trinity_out_dir/Trinity.fasta
+20190409_trinity_pgen_EPI99_RNAseq/trinity_out_dir/Trinity.fasta)
