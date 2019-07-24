3152468e4aabac8350e100096a7895f95f939772
sam <samwhite@uw.edu>
2019-07-24 14:38:06 -0700

    initial commit

---
+++ bash_scripts/20190729_cdhit_pgen_trinity_assemblies.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+# Script to retrieve geoduck Trinity assemblies
+# Assemblies will be used in SBATCH script called at end of this script.
+
+assembly_dirs=(
+/gscratch/srlab/sam/data/P_generosa/transcriptomes/20190827_assembly \
+/gscratch/srlab/sam/data/P_generosa/transcriptomes/ctenidia \
+/gscratch/srlab/sam/data/P_generosa/transcriptomes/gonad \
+/gscratch/srlab/sam/data/P_generosa/transcriptomes/heart \
+/gscratch/srlab/sam/data/P_generosa/transcriptomes/juvenile/EPI115 \
+/gscratch/srlab/sam/data/P_generosa/transcriptomes/juvenile/EPI116 \
+/gscratch/srlab/sam/data/P_generosa/transcriptomes/juvenile/EPI123 \
+/gscratch/srlab/sam/data/P_generosa/transcriptomes/juvenile/EPI124 \
+/gscratch/srlab/sam/data/P_generosa/transcriptomes/larvae/EPI99)
