1e0967492b1a124ca0d6d65e3b353004615d25b1
Sam White <samuel.j.white@gmail.com>
2019-07-29 11:24:15 -0700

    change 20180827 assembly date to be accurate

--- bash_scripts/20190729_rsync_pgen_trinity_assemblies.sh
+++ bash_scripts/20190729_rsync_pgen_trinity_assemblies.sh
@@ -13,7 +13,7 @@ owl="owl:/volume1/web/Athaliana"
 
 # Create array of directories for storing Trinity assemblies
 assembly_dirs_array=(
-/gscratch/srlab/sam/data/P_generosa/transcriptomes/20190827_assembly
+/gscratch/srlab/sam/data/P_generosa/transcriptomes/20180827_assembly
 /gscratch/srlab/sam/data/P_generosa/transcriptomes/ctenidia
 /gscratch/srlab/sam/data/P_generosa/transcriptomes/gonad
 /gscratch/srlab/sam/data/P_generosa/transcriptomes/heart
