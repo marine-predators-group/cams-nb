490bc19d9fbc735d9511aff09fbfca637282b082
Sam White <samuel.j.white@gmail.com>
2019-08-06 13:54:14 -0700

    add nucmer command

--- sbatch_scripts/20190806_pgen_mummer_pgen-v074_cvir-ncbi.sh
+++ sbatch_scripts/20190806_pgen_mummer_pgen-v074_cvir-ncbi.sh
@@ -57,3 +57,9 @@ cvir_fasta="/gscratch/srlab/sam/data/C_virginica/genomes/GCF_002022765.2_C_virgi
 pgen_v074_fasta="/gscratch/srlab/sam/data/P_generosa/genomes/Pgenerosa_v074.fa"
 
 ### Run MUMmer (nucmer)
+# Compares pgen_v074 (query) to cvir-ncbi (reference)
+"${nucmer}" \
+"${cvir_fasta}" \
+"${pgen_v074_fasta}" \
+--prefix="${prefix}" \
+--threads="${threads}"
