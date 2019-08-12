2893d5b57ab3e2ce810fd4acff9560f683bbaef0
Sam White <samuel.j.white@gmail.com>
2019-08-12 15:04:42 -0700

    fix quoting to pass shellcheck.net

--- bash_scripts/sbatch_job_submission.sh
+++ bash_scripts/sbatch_job_submission.sh
@@ -11,11 +11,11 @@
 sbatch_script="$1"
 
 # Extract account and partition names from suppllied SBATCH script.
-account=$(grep "\-\-account" ${sbatch_script} | awk -F "=" '{print $2}')
-partition=$(grep "\-\-partition" ${sbatch_script} | awk -F "=" '{print $2}')
+account=$(grep "\-\-account" "${sbatch_script}" | awk -F "=" '{print $2}')
+partition=$(grep "\-\-partition" "${sbatch_script}" | awk -F "=" '{print $2}')
 
 # Execute SBATCH job submission.
 sbatch \
--p ${partition} \
--A ${account} \
-${sbatch_script}
+-p "${partition}" \
+-A "${account}" \
+"${sbatch_script}"
