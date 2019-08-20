bee1c07ad121ee2f8c172188e9aa12765f484447
Sam White <samuel.j.white@gmail.com>
2019-08-20 13:40:24 -0700

    add rsync commands for downloading files

--- bash_scripts/20190820_pgen_mummer_dot_analysis.sh
+++ bash_scripts/20190820_pgen_mummer_dot_analysis.sh
@@ -2,3 +2,19 @@
 
 # Script for downloading Pgenerosa_v074 MUMMER delta files and creating "Dot" coordinate files
 # for visualization: https://dnanexus.github.io/dot/
+
+nucmer_delta_rsync="/volume2/web/Atumefaciens/20190807*/*.delta"
+promer_delta_rsync="/volume2/web/Atumefaciens/20190813*/*.delta"
+
+# Make output directories
+mkdir nucmer_delta promer_delta
+
+# Download nucloetide MUMMER delta files
+cd "${nucmer_delta}" \
+&& rsync -av --progress "${nucmer_delta_rsync}" .
+
+
+
+# Download nucloetide MUMMER delta files
+cd ../"${promer_delta}" \
+&& rsync -av --progress "${promer_delta_rsync}" .
