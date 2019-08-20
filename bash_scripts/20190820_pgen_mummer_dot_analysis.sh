8d81b8e1c3fe5e6b71ad2b80c1ecb3dbeaece557
Sam White <samuel.j.white@gmail.com>
2019-08-20 14:46:37 -0700

    fix quoting

--- bash_scripts/20190820_pgen_mummer_dot_analysis.sh
+++ bash_scripts/20190820_pgen_mummer_dot_analysis.sh
@@ -4,6 +4,9 @@
 # for visualization: https://dnanexus.github.io/dot/
 
 
+# Capture date. E.g. format is: 20190820
+timestamp=$(date +"%Y%m%d")
+
 nucmer_delta_rsync="/volume2/web/Atumefaciens/20190807*/*.delta"
 promer_delta_rsync="/volume2/web/Atumefaciens/20190813*/*.delta"
 
@@ -24,12 +27,14 @@ cd ../"${promer_delta}" \
 # Looop through directories and run DotPrep.py on each delta file
 for folder in *delta/
 do
+	mummer=$(echo "${folder}" | awk -F"_" '{ print $1 }')
 	cd "${folder}"
   for delta in *.delta
 	do
+		comparison=$(echo "${delta}" | awk -F"." '{ print $1 }')
 		../DotPrep.py \
 		--delta "${delta}" \
-		--out "${timestamp}"_"${dir}"_${comparison}.coords.dot
+		--out "${timestamp}"_"${mummer}"_"${comparison}".coords.dot
 	done
 	cd ..
 done
