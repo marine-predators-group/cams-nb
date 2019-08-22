3090f3eba8a129a3c6b31a6c5b531f5c5fe83d72
Sam White <samuel.j.white@gmail.com>
2019-08-22 09:37:41 -0700

    initial commit

---
+++ sbatch_scripts/20190822_crog_sibelia.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+## Job Name
+#SBATCH --job-name=sibelia
+## Allocation Definition
+#SBATCH --account=srlab
+#SBATCH --partition=srlab
+## Resources
+## CPU request
+#SBATCH --constraint="skylake"
+## Nodes
+#SBATCH --nodes=1
+## Walltime (days-hours:minutes:seconds format)
+#SBATCH --time=30-00:00:00
+## Memory per node
+#SBATCH --mem=500G
+##turn on e-mail notification
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=samwhite@uw.edu
+## Specify the working directory for this job
+#SBATCH --chdir=/gscratch/scrubbed/samwhite/outputs/20190822_crog_sibeliea
+
+
+
+# Set working dir
+wd=$(pwd)
+
+
+# Input/output files
+assembly_fasta="${wd}/Caligus_rogercresseyi_Genome.fa"
+assembly_index="${wd}/Caligus_rogercresseyi_Genome.fa.fai"
+assembly_subset="${wd}/C_rogercresseyi_top_21_scaffold.fa"
+system_specs="${wd}/system_info.txt"
+subset_list="${wd}/top_21_scaffold_list.txt"
+
+# Program paths
+samtools="/gscratch/srlab/programs/samtools-1.9/samtools"
+sibelia="/gscratch/srlab/programs/Sibelia-3.0.7-Source/Sibelia"
+
+
+# Record system info
+echo "TODAY'S DATE:" >> "${system_specs}"
+date >> "${system_specs}"
+echo "------------" >> "${system_specs}"
+echo "" >> "${system_specs}"
+lsb_release -a >> "${system_specs}"
+echo "" >> "${system_specs}"
+echo "------------" >> "${system_specs}"
+echo "HOSTNAME: " hostname  >> "${system_specs}"
+echo "" >> "${system_specs}"
+echo "------------" >> "${system_specs}"
+echo "Computer Specs:" >> "${system_specs}"
+echo "" >> "${system_specs}"
+lscpu >> "${system_specs}"
+echo "" >> "${system_specs}"
+echo "------------" >> "${system_specs}"
+echo "" >> "${system_specs}"
+echo "Memory Specs" >> "${system_specs}"
+echo "" >> "${system_specs}"
+free -mh >> "${system_specs}"
+
+# Index FastA file
+"${samtools}" faidx \
+"${assembly_fasta}"
+
+# Extract list of top 21 longest scaffolds
+sort \
+--numeric-sort \
+--reverse \
+--field-separator $'\t' \
+--key 2,2 \
+"${assembly_index}" \
+| head -n 21 \
+| awk -F'\t' '{print $1}' \
+> "${subset_list}"
+
+# Create FastA of longest 21 scaffolds
+xargs \
+"${samtools}" faidx \
+"${assembly_fasta}" \
+< "${subset_list}" \
+> "${assembly_subset}"
+
+# Index new FastA
+"${samtools}" faidx \
+"${assembly_subset}"
+
+# Run Sibelia
+{ "${sibelia}" \
+--parameters loose \
+--outdir "${wd}" \
+"${assembly_subset}" \
+2> sibelia_stderr.txt
