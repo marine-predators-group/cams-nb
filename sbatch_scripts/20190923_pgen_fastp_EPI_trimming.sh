e133fda93481b3e80e325ccff8e977a5baf0d94d
Sam White <samuel.j.white@gmail.com>
2019-09-23 12:24:01 -0700

    add threads to fastp command; fix quoting

--- sbatch_scripts/20190923_pgen_fastp_EPI_trimming.sh
+++ sbatch_scripts/20190923_pgen_fastp_EPI_trimming.sh
@@ -83,13 +83,14 @@ for index in "${!fastq_array_R1[@]}"
 do
   sample_name=$(echo "${names_array[index]}")
 	${fastp} \
-	--in1 ${fastq_array_R1[index]} \
-	--in2 ${fastq_array_R2[index]} \
+	--in1 "${fastq_array_R1[index]}" \
+	--in2 "${fastq_array_R2[index]}" \
 	--disable_quality_filtering \
 	--disable_length_filtering \
 	--disable_adapter_trimming \
 	--trim_front1 ${num_nucs_trim} \
 	--trim_front2 ${num_nucs_trim} \
-	--out1 ${sample_name}.20bp-trim.fq.gz \
-	--out2 ${sample_name}.20bp-trim.fq.gz
+	--thread ${threads}
+	--out1 "${sample_name}".20bp-trim.fq.gz \
+	--out2 "${sample_name}".20bp-trim.fq.gz
 done
