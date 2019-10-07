a439272ed5247dc788cdaede85382aa4d2335a0e
sam <samwhite@uw.edu>
2019-10-07 11:40:15 -0700

    add mapping file paths/URL

--- bash_scripts/20191007_metagenomics_meganizer.sh
+++ bash_scripts/20191007_metagenomics_meganizer.sh
@@ -2,3 +2,14 @@
 
 # Script to run MEGAN6 meganizer on DIAMOND DAA files from
 # 20190925_metagenomics_DIAMOND_blastx Mox job.
+
+# Requires MEGAN mapping files from:
+# http://ab.inf.uni-tuebingen.de/data/software/megan6/download/
+
+# Program path
+meganizer=/home/sam/programs/megan/tools/daa-meganizer
+
+# MEGAN mapping files
+prot_acc2tax=/home/sam/data/databases/MEGAN/prot_acc2tax-Jul2019X1.abin
+acc2interpro=/home/sam/data/databases/MEGAN/acc2interpro-Jul2019X.abin
+acc2eggnog=/home/sam/data/databases/MEGAN/acc2eggnog-Jul2019X.abin
