ef0906c295d32ecb81760bad9a4792a002e43321
sam <samwhite@uw.edu>
2019-10-07 11:48:26 -0700

    add meganizer loop

--- bash_scripts/20191007_metagenomics_meganizer.sh
+++ bash_scripts/20191007_metagenomics_meganizer.sh
@@ -13,3 +13,13 @@ meganizer=/home/sam/programs/megan/tools/daa-meganizer
 prot_acc2tax=/home/sam/data/databases/MEGAN/prot_acc2tax-Jul2019X1.abin
 acc2interpro=/home/sam/data/databases/MEGAN/acc2interpro-Jul2019X.abin
 acc2eggnog=/home/sam/data/databases/MEGAN/acc2eggnog-Jul2019X.abin
+
+# Run MEGANIZER
+for daa in *.daa
+do
+  ${meganizer} \
+  --in "${daa}" \
+	--acc2taxa ${prot_acc2tax} \
+	--acc2interpro2go ${acc2interpro} \
+	--acc2eggnog ${acc2eggnog}
+done
