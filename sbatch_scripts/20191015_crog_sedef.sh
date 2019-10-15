fcca0927facfc1b0832db875650a54b731faa4d4
Sam White <samuel.j.white@gmail.com>
2019-10-15 11:41:20 -0700

    initial commit

---
+++ sbatch_scripts/20191015_crog_sedef.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+## Job Name
+#SBATCH --job-name=sedef
+## Allocation Definition
+#SBATCH --account=srlab
+#SBATCH --partition=srlab
+## Nodes
+#SBATCH --nodes=1
+## Walltime (days-hours:minutes:seconds format)
+#SBATCH --time=20-00:00:00
+## Memory per node
+#SBATCH --mem=500G
+##turn on e-mail notification
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=samwhite@uw.edu
+## Specify the working directory for this job
+#SBATCH --chdir=/gscratch/scrubbed/samwhite/outputs/20191015_crog_sedef
+
+
+
+# Set working dir
+wd=$(pwd)
+
+
+# Input/output files
+assembly_fasta="${wd}/Caligus_rogercresseyi_Genome.fa"
+assembly_index="${wd}/Caligus_rogercresseyi_Genome.fa.fai"
+assembly_subset="${wd}/C_rogercresseyi_top_21_scaffold.fa"
+system_specs="${wd}/system_info.txt"
+subset_list="${wd}/top_21_scaffold_list.txt"
+
+# Program paths
+samtools="/gscratch/srlab/programs/samtools-1.9/samtools"
+sibelia="/gscratch/srlab/programs/sedef/sedef.sh"
+
+
+# Record system info
+{
+echo "TODAY'S DATE:"
+date
+echo "------------"
+echo ""
+lsb_release -a
+echo ""
+echo "------------"
+echo "HOSTNAME: " hostname
+echo ""
+echo "------------"
+echo "Computer Specs:"
+echo ""
+lscpu
+echo ""
+echo "------------"
+echo ""
+echo "Memory Specs"
+echo ""
+free -mh
+} >> "${system_specs}"
+
+
+# Index assembly FastA file
+"${samtools}" faidx \
+"${assembly_fasta}"
+
+# Extract list of top 21 longest scaffolds
+sort \
+--numeric-sort \
+--reverse \
+--field-separator $'\t' \
+--key 2,2 \
+"${assembly_index}" \
+| head -n 21 \
+| awk -F'\t' '{print $1}' \
+> "${subset_list}"
+
+# Create FastA of longest 21 scaffolds
+xargs \
+"${samtools}" faidx \
+"${assembly_fasta}" \
+< "${subset_list}" \
+> "${assembly_subset}"
+
+# Index new FastA
+"${samtools}" faidx \
+"${assembly_subset}"
+
+# Run SEDEF
