2bfc49d3d1b1ef53b690113e00bdcfab0406ff35
Sam White <samuel.j.white@gmail.com>
2019-10-16 09:58:28 -0700

    add rsync commands

--- bash_scripts/20191016_pgen_drap.sh
+++ bash_scripts/20191016_pgen_drap.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 
+# All RNAseq FastQ files are available here:
+# https://owl.fish.washington.edu/nightingales/P_generosa/
+
+
+
+rsync_path="owl:/volume1/web/nightingales/P_generosa"
+
+
 
 tissues_array=(ctenidia gonad heart juvenile larvae)
-
 treatment_array=(ambient OA)
 
 
-
+# Make directories
 for tissue in ${tissues_array[@]}
 do
 	if [ "${tissue}" == "juvenile" ] \
@@ -20,3 +27,7 @@ do
 		mkdir ${tissue}
 	fi
 done
+
+# Download files
+rsync -av --progress "${rsync_path}"/*RNA*.gz .
+rsync -av --progress "${rsync_path}"/NR0[012][12569]*.gz .
