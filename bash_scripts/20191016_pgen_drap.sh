6afcb804607680a5aa88e0af730c9954dfe77af5
Sam White <samuel.j.white@gmail.com>
2019-10-22 11:34:06 -0700

    rm unused arrays

--- bash_scripts/20191016_pgen_drap.sh
+++ bash_scripts/20191016_pgen_drap.sh
@@ -11,14 +11,10 @@ rsync -av --progress "${rsync_path}"/NR0[012][12569]*.gz .
 rsync_path="owl:/volume1/web/nightingales/P_generosa"
 
 
-
+# Set array values for tissue, treatment, and "NR" filenames
 tissues_array=(ctenidia gonad heart juvenile larvae)
 treatment_array=(ambient OA)
 nr_files_array=(NR005 NR006 NR012 NR015 NR019 NR021)
-tissue_files_array=()
-treatement_files_array=()
-
-
 
 
 # Make directories
