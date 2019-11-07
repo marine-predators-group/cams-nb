e42d50e54aa8089c6509c99a583b3e05b19adcde
Sam White <samuel.j.white@gmail.com>
2019-11-07 09:39:54 -0800

    complete while karyotype chromosome definition loop

--- bash_scripts/circos_pgen_karyotype.sh
+++ bash_scripts/circos_pgen_karyotype.sh
@@ -8,7 +8,9 @@ fasta_index=${HOME}/data/P_generosa/genomes/Panopea-generosa-v1.0.fa.fai
 karyo_file=karyotype.pg.txt
 species=pg
 
-while read -r line
+while IFS=$'\t' read -r line
 do
-	awk -v var="${species}" '{print $1}' "${line}"
-done <<< "${fasta_index}"
+	scaffold_num=$(echo "${line}" | awk -F"[_\t]" '{print $2}' | sed 's/^0//')
+	scaffold_length=$(echo "${line}" | awk '{print $2}')
+	printf "chr - %s\n" "${species}${scaffold_num}${scaffold_num} 0 ${scaffold_length} chr${scaffold_num}"
+done < "${fasta_index}"
