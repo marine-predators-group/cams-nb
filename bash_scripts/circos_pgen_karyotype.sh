81f68482b6e55a09f0ff0897fab2d8919a0e0928
Sam White <samuel.j.white@gmail.com>
2019-11-07 08:06:55 -0800

    set variables, initiate while loop

--- bash_scripts/circos_pgen_karyotype.sh
+++ bash_scripts/circos_pgen_karyotype.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 # Script to create karyotype file for Circos
+
+
+# Variables
+fasta_index=${HOME}/data/P_generosa/genomes/Panopea-generosa-v1.0.fa.fai
+karyo_file=karyotype.pg.txt
+species=pg
+
+while read -r line
+do
+	awk -v var="${species}" '{print $1}' "${line}"
+done <<< "${fasta_index}"
