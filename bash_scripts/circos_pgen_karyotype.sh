38c8c3a4c8d098ebbdb003e74f5d35c527732b36
Sam White <samuel.j.white@gmail.com>
2019-11-07 09:40:41 -0800

    fix spacing in printf statement

--- bash_scripts/circos_pgen_karyotype.sh
+++ bash_scripts/circos_pgen_karyotype.sh
@@ -12,5 +12,5 @@ while IFS=$'\t' read -r line
 do
 	scaffold_num=$(echo "${line}" | awk -F"[_\t]" '{print $2}' | sed 's/^0//')
 	scaffold_length=$(echo "${line}" | awk '{print $2}')
-	printf "chr - %s\n" "${species}${scaffold_num}${scaffold_num} 0 ${scaffold_length} chr${scaffold_num}"
+	printf "chr - %s\n" "${species}${scaffold_num} ${scaffold_num} 0 ${scaffold_length} chr${scaffold_num}"
 done < "${fasta_index}"
