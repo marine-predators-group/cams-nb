b612b397f8f300806c53d224fee424eaea407c93
Sam White <samuel.j.white@gmail.com>
2019-11-08 06:44:57 -0800

    add output redirection

--- bash_scripts/circos_pgen_karyotype.sh
+++ bash_scripts/circos_pgen_karyotype.sh
@@ -13,4 +13,4 @@ do
 	scaffold_num=$(echo "${line}" | awk -F"[_\t]" '{print $2}' | sed 's/^0//')
 	scaffold_length=$(echo "${line}" | awk '{print $2}')
 	printf "chr - %s\n" "${species}${scaffold_num} ${scaffold_num} 0 ${scaffold_length} chr${scaffold_num}"
-done < "${fasta_index}"
+done < "${fasta_index}" >> "${karyo_file}"
